2045d91386d97ccf27b594de6d8b66bc408b40b0
Lucas-Duarte-dev <Lucasduarteb2000@hotmail.com>
2021-12-06 21:31:57 -0300

    refactor: alterando a conexão com a base

--- back_end/src/infra/typeorm/index.ts
+++ back_end/src/infra/typeorm/index.ts
@@ -1,10 +1,6 @@
 import {createConnection, getConnectionOptions, Connection} from "typeorm";
 
 export default async(host = "postgressql.amalteia.docker"): Promise<Connection> => {
-    const defaultOptions = await getConnectionOptions();
-    return createConnection(
-        Object.assign(defaultOptions, {
-            host
-        }
-    ));
+    const defaultOptions = await getConnectionOptions("development" );
+    return createConnection({ ...defaultOptions, name: "development" } as any);
 }
