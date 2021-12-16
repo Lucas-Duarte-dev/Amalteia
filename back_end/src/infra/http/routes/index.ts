5920603710c0207dee3c0d35a70c68a6c44cd821
Lucas-Duarte-dev <Lucasduarteb2000@hotmail.com>
2021-12-15 22:15:59 -0300

    feat(http): puxando a rota de categorias

--- back_end/src/infra/http/routes/index.ts
+++ back_end/src/infra/http/routes/index.ts
@@ -1,8 +1,10 @@
 import {Router} from "express";
 import {accountRoutes} from "@infra/http/routes/account.routes";
+import {categoryRoutes} from "@infra/http/routes/category.routes";
 
 const routes = Router();
 
-routes.use("/account", accountRoutes);
+routes.use('/account', accountRoutes);
+routes.use('/category', categoryRoutes);
 
 export {routes};
