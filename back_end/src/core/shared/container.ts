d7483277c3cd04c034957c7a8e1004ca7c8c75b3
Lucas-Duarte-dev <Lucasduarteb2000@hotmail.com>
2021-12-17 21:31:39 -0300

    refactor: adicionando o campo password_confirmed

--- back_end/src/core/shared/container.ts
+++
@@ -1,8 +0,0 @@
-import {container} from "tsyringe";
-import {IUserRepository} from "@module/account/repositories/IUserRepository";
-import {UserRepository} from "@module/account/repositories/typeorm/UserRepository";
-
-container.registerSingleton<IUserRepository>(
-    "UserRepository",
-    UserRepository
-);
--- back_end/src/modules/account/dto/ICreateUserDTO.ts
+++ back_end/src/modules/account/dto/ICreateUserDTO.ts
@@ -2,4 +2,5 @@ export interface ICreateUserDTO {
     name: string,
     email: string,
     password: string,
+    password_confirmed: string
 }
