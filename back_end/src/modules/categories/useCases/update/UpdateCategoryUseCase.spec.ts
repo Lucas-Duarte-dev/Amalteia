716aa2a913be09cab2124eaf57b3dc4567f1f621
Lucas-Duarte-dev <Lucasduarteb2000@hotmail.com>
2021-12-19 21:01:27 -0300

    test: criando teste para verificar se é possível atualizar o nome de uma categoria
    criando teste para verificar se não é possível alterar o nome se a categoria não existir

--- back_end/src/modules/categories/useCases/update/UpdateCategoryUseCase.spec.ts
+++ back_end/src/modules/categories/useCases/update/UpdateCategoryUseCase.spec.ts
@@ -1,5 +1,6 @@
 import {CreateCategoryUseCase} from "@module/categories/useCases/register/CreateCategoryUseCase";
 import {CategoryRepository} from "@module/categories/repositories/in-memory/CategoryRepository";
+import {UpdateCategoryUseCase} from "@module/categories/useCases/update/UpdateCategoryUseCase";
 
 
 let categoryRepository = new CategoryRepository();
@@ -13,14 +14,27 @@ describe('Update Category', () => {
 
         createCategory = new CreateCategoryUseCase(categoryRepository);
 
-        updateCategory = new UpdateCategoryUseCase();
+        updateCategory = new UpdateCategoryUseCase(categoryRepository);
+
+        await createCategory.execute({
+            name: 'ciencia',
+            description: 'tema de ciencia'
+        });
     });
 
    it('Should be able change category name', async () => {
        const expected = 'biologia';
 
-       const category = await updateCategory.execute(expected);
+       const category = await updateCategory.execute('ciencia', expected);
 
        expect(category.name).toEqual(expected);
    });
+
+   it('Should no be able change category name when old name passed not exist',() => {
+       const expected = {message: 'Category not found', statusCode: 400};
+
+       expect(async () => {
+           await updateCategory.execute('artes', 'biologia');
+       }).rejects.toEqual(expected);
+   })
 });
