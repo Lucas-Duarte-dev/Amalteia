4153e32978423393220687b9abfcf88d4a10ea17
Lucas-Duarte-dev <Lucasduarteb2000@hotmail.com>
2021-12-20 22:07:06 -0300

    feat(module): criando a rota de atualização de categorias

--- back_end/src/infra/http/routes/category.routes.ts
+++ back_end/src/infra/http/routes/category.routes.ts
@@ -1,9 +1,11 @@
 import {Router} from "express";
 import {adaptRoute} from "@core/infra/adapter/ExpressRouteAdapter";
 import makeCreateCategoryController from "@infra/http/factory/controller/makeCreateCategoryController";
+import makeUpdateCategoryController from "@infra/http/factory/controller/makeUpdateCategoryController";
 
 const categoryRoutes = Router();
 
 categoryRoutes.post('/', adaptRoute(makeCreateCategoryController()));
+categoryRoutes.put('/', adaptRoute(makeUpdateCategoryController()));
 
 export {categoryRoutes};
