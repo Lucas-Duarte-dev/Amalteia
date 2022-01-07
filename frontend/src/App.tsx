59653bd7c83acab5d7eadee1442cfb782a97fb2d
Lucas-Duarte-dev <Lucasduarteb2000@hotmail.com>
2022-01-06 23:23:00 -0300

    feat(page): adicionando o componente Header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Header} from "./domain/components/Header";
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+        <h1>Hello World</h1>
+        <Header />
     </div>
   );
 }
